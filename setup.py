3bec189cd2b339322d45f1884d7decc5d390c35e
gilh <nsgil85@gmail.com>
2022-12-21 13:33:22 +0200

    1.1.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='VideoToolBox',
-    version='1.1.0',    
+    version='1.1.1',    
     description='A video processing package',
     url='https://github.com/nsgil85/VideoToolBox',
     author='Gil h',
     author_email='nsgil85@gmail.com',
     license='BSD 2-clause',
     packages=['VideoToolBox'],
-    install_requires=['numpy'],
+    install_requires=['numpy','opencv-python],
 
     classifiers=[
         'Development Status :: 1 - Planning',
